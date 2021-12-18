805f5b6188bbe5e73a216a68b84aff8444025db7
uglyonlytoday <superjang3357@naver.com>
2021-12-18 22:35:39 +0900

    기초 pages, navigation

---
+++ lib/pages/reactive.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ReactivePage extends StatelessWidget {
+  const ReactivePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Reactive way')),
+      body: Container(),
+    );
+  }
+}
